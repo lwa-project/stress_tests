c7fea99e4e06a968b0ebbc8452d2dbe5c526bef8
jaycedowell <jayce.dowell@gmail.com>
2020-09-22 13:42:41 -0600

    Dropped Python2 support.

--- observationTimes.py
+++ observationTimes.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """
 Script to generate a list of times that a given source is at a variety of 
@@ -8,92 +8,22 @@ Usage:
   observationTimes.py <source name> YYYY/MM/DD
 """
 
-from __future__ import print_function, division
-
 import sys
 import ephem
 import numpy
-import getopt
+import argparse
 from datetime import datetime
 
 from lsl.common import stations
+from lsl.misc import parser as aph
 
 from analysis import getSources
 
 
-def usage(exitCode=None):
-    print("""observationTimes.py - Script to generate a list of times that a given 
-source is at a variety of elevations (30 to 90 degrees, plus transit) for 
-a given UTC day.
-
-Usage:
-observationTimes.py [OPTIONS] <source name> YYYY/MM/DD
-
-Options:
--h, --help                  Display this help information
--v, --lwassv                Compute for LWA-SV instead of LWA-1
--l, --list                  List valid sources and exit
--e, --elevations            Comma separated list of additional 
-                            elevations in degrees to search for
-""")
-    
-    if exitCode is not None:
-        sys.exit(exitCode)
-    else:
-        return True
-
-
-def parseOptions(args):
-    config = {}
-    # Command line flags - default values
-    config['site'] = 'lwa1'
-    config['list'] = False
-    config['elevations'] = [30, 40, 50, 60, 70, 80, 90]
-    
-    # Read in and process the command line flags
-    try:
-        opts, args = getopt.getopt(args, "hvle:", ["help", "lwasv", "list", "elevations="])
-    except getopt.GetoptError as err:
-        # Print help information and exit:
-        print(str(err)) # will print something like "option -a not recognized"
-        usage(exitCode=2)
-        
-    # Work through opts
-    for opt, value in opts:
-        if opt in ('-h', '--help'):
-            usage(exitCode=0)
-        elif opt in ('-v', '--lwasv'):
-            config['site'] = 'lwasv'
-        elif opt in ('-e', '--elevations'):
-            fields = value.split(',')
-            fields = [float(f) for f in fields]
-            for f in fields:
-                if f not in config['elevations']:
-                    config['elevations'].append(f)
-        elif opt in ('-l', '--list'):
-            config['list'] = True
-        else:
-            assert False
-            
-    # Add in arguments and make sure there are enough
-    config['args'] = args
-    if not config['list'] and len(config['args']) != 2:
-        raise RuntimeError("Must specify a source name and UTC date")
-        
-    # Sort the list of elevations
-    config['elevations'].sort()
-    
-    # Return configuration
-    return config
-
-
 def main(args):
-    # Parse command line options
-    config = parseOptions(args)
-    
     # Load in the sources and list if needed
     srcs = getSources()
-    if config['list']:
+    if args.list:
         print("Valid Sources:")
         print(" ")
         print("%-8s  %11s  %11s  %6s" % ("Name", "RA", "Dec", "Epoch"))
@@ -103,9 +33,11 @@ def main(args):
         sys.exit()
         
     # Break out what we need from the arguments
-    srcName = config['args'][0]
-    date    = config['args'][1]
-    
+    srcName = args.source
+    date    = args.date
+    if srcName is None or date is None:
+        raise RuntimeError("Need both a source name and a UTC date")
+        
     # Find the right source
     toUse = None
     for src in srcs.keys():
@@ -114,7 +46,7 @@ def main(args):
             
     # Get the observer
     obs = stations.lwa1.get_observer()
-    if config['site'] == 'lwasv':
+    if args.lwasv:
         obs = stations.lwasv.get_observer()
         
     if toUse is None:
@@ -124,10 +56,10 @@ def main(args):
     
     tRise = {}
     tSet = {}
-    for el in config['elevations']:
+    for el in args.elevations:
         ## Reset the data and horizon
         obs.date = "%s 00:00:00.000000" % date
-        obs.horizon = ephem.degrees(str(el))
+        obs.horizon = el
         
         ## Rise time
         try:
@@ -157,13 +89,13 @@ def main(args):
     print("%s on %s UTC:" % (srcs[toUse].name, date))
     
     print("  rising")
-    for el in config['elevations']:
+    for el in args.elevations:
         try:
             t = tRise[el]
             obs.date = t
             srcs[toUse].compute(obs)
             
-            print("    el: %4.1f degrees at %s (el: %4.1f, az: %5.1f)" % (el, t, srcs[toUse].alt*180/numpy.pi, srcs[toUse].az*180/numpy.pi))
+            print("    el: %4.1f degrees at %s (el: %4.1f, az: %5.1f)" % (el*180/numpy.pi, t, srcs[toUse].alt*180/numpy.pi, srcs[toUse].az*180/numpy.pi))
         except KeyError:
             pass
             
@@ -173,17 +105,32 @@ def main(args):
     print("    el: %4.1f degrees at %s" % (srcs[toUse].alt*180/numpy.pi, tTransit))
     
     print("  setting")
-    for el in config['elevations'][::-1]:
+    for el in args.elevations[::-1]:
         try:
             t = tSet[el]
             obs.date = t
             srcs[toUse].compute(obs)
             
-            print("    el: %4.1f degrees at %s (el: %4.1f, az: %5.1f)" % (el, t, srcs[toUse].alt*180/numpy.pi, srcs[toUse].az*180/numpy.pi))
+            print("    el: %4.1f degrees at %s (el: %4.1f, az: %5.1f)" % (el*180/numpy.pi, t, srcs[toUse].alt*180/numpy.pi, srcs[toUse].az*180/numpy.pi))
         except KeyError:
             pass
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    parser = argparse.ArgumentParser(
+        description='script to generate a list of times that a given source is at a variety of elevations (30 to 90 degrees, plus transit) for a given UTC day',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+        )
+    parser.add_argument('source', type=str, nargs='?',
+                        help='source name to compute times for')
+    parser.add_argument('date', type=aph.date, nargs='?',
+                        help='UTC date to compute times for')
+    parser.add_argument('-v', '--lwasv', action='store_true',
+                        help='compute for LWA-SV instead of LWA1')
+    parser.add_argument('-l', '--list', action='store_true',
+                        help='list valid source names and exit')
+    parser.add_argument('-e', '--elevations', type=aph.csv_degrees_list, default=[ephem.degrees('%i' % v) for v in range(30, 100, 10)],
+                        help='comma separated list of additional elevations in degrees to search for')
+    args = parser.parse_args()
+    main(args)
     
