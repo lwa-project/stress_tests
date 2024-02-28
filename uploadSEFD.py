73a8aa004693510c06ee7dca4244405e4d870131
jaycedowell <jaycedowell@gmail.com>
2024-02-28 15:47:38 -0700

    Switch to JSON here as well.

--- uploadSEFD.py
+++ uploadSEFD.py
@@ -2,11 +2,20 @@
 
 import os
 import sys
+import json
+from datetime import datetime
 
 from lwa_auth import KEYS as LWA_AUTH_KEYS
 from lwa_auth.signed_requests import post as signed_post
 
 URL = "https://lwalab.phys.unm.edu/OpScreen/update"
+
+
+def _serialize_datetime(value):
+    try:
+        return value.isoformat()
+        except AttributeError:
+    return value
 
 
 def main(args):
@@ -15,12 +24,22 @@ def main(args):
             for line in fh:
                 line = line.strip().rstrip()
             line = line.split()
-            line = ';;;'.join(line)
             
+            data = []
+            data.append({'source':     line[0],
+                         'zenith_ang': line[3],
+                         'frequency':  float(line[4]),
+                         'err_ra':     line[5],
+                         'err_dec':    line[6],
+                         'sefd':       float(line[7]),
+                         'fwhm':       line[8],
+                         'updated':    datetime.strptime(f"{line[1]} {line[2]}", "%Y/%m/%d %H:%M:%S")})
+            
+            out = json.dumps(data, default=_serialize_datetime)
             f = signed_post(LWA_AUTH_KEYS.get('lwaucf', kind='private'), URL,
-                            data={'site': site, 'subsystem': 'SEFD', 'data': line})
+                            data={'site': site, 'subsystem': 'SEFD', 'data': out})
             f.close()
-
+            
     p = os.path.dirname(os.path.abspath(__file__))
     os.system(os.path.join(p, 'influxSEFD.py'))
     os.system(os.path.join(p, 'influxSEFD_SV.py'))
