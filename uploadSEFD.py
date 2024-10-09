53cf68850f9c6f5237554cf09bff3b2e1573d135
jaycedowell <jaycedowell@gmail.com>
2024-10-09 13:48:15 -0600

    This is already handled by runAutoProcess.py (#7).

--- uploadSEFD.py
+++ uploadSEFD.py
@@ -44,11 +44,6 @@ def main(args):
             f = signed_post(LWA_AUTH_KEYS.get('lwaucf', kind='private'), URL,
                             data={'site': site, 'subsystem': 'SEFD', 'data': out})
             f.close()
-            
-    p = os.path.dirname(os.path.abspath(__file__))
-    os.system(os.path.join(p, 'influxSEFD.py'))
-    os.system(os.path.join(p, 'influxSEFD_SV.py'))
-    os.system(os.path.join(p, 'influxSEFD_NA.py'))
 
 
 if __name__ == '__main__':
